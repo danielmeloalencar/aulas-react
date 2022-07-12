57281d5d2e01755299df4e2f5d9b50d9294b4e66
Daniel Alencar <danielmalencar2011@gmail.com>
2022-07-12 19:32:33 -0300

    UseState e OnClick

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 
 function App() {
+  const [nome, setNome] = useState()               
+  
+  function handleClick(){
+    setNome("Daniel Alencar")
+  }
 
   return (
     <div>
-      
+      <p>{nome}</p>
+      <div onClick={handleClick} >Mudar nome</div>
     </div>
   );
 }
